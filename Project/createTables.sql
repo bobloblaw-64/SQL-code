4d6ed60ae0c5b9453c9721f3c71404d50e7c3a07
bobloblaw-64 <fkthomson01@gmail.com>
2026-09-23 15:24:10 +0800

    better checksum

--- Project/createTables.sql
+++ Project/createTables.sql
@@ -11,7 +11,26 @@ CREATE TABLE Member (
     memberId INTEGER PRIMARY KEY NOT NULL,
     memberName TEXT,
     memberEmail TEXT,
-    cardNumber TEXT,
+    cardNumber TEXT CHECK (
+        cardNumber IS NULL OR (
+            length(cardNumber) = 10
+            
+            AND cardNumber NOT GLOB '*[^0-9]*'
+            
+            AND (
+                10 * CAST(substr(cardNumber, 1, 1) AS INTEGER) +
+                9 * CAST(substr(cardNumber, 2, 1) AS INTEGER) +
+                8 * CAST(substr(cardNumber, 3, 1) AS INTEGER) +
+                7 * CAST(substr(cardNumber, 4, 1) AS INTEGER) +
+                6 * CAST(substr(cardNumber, 5, 1) AS INTEGER) +
+                5 * CAST(substr(cardNumber, 6, 1) AS INTEGER) +
+                4 * CAST(substr(cardNumber, 7, 1) AS INTEGER) +
+                3 * CAST(substr(cardNumber, 8, 1) AS INTEGER) +
+                2 * CAST(substr(cardNumber, 9, 1) AS INTEGER) +
+                1 * CAST(substr(cardNumber, 10, 1) AS INTEGER)
+            ) % 11 = 0
+        )
+    ),
     joinDate TEXT
 );
 
@@ -67,55 +86,3 @@ CREATE TABLE Recipe (
     FOREIGN KEY (productId) REFERENCES Product (productId) ON DELETE CASCADE,
     FOREIGN KEY (ingredientId) REFERENCES Ingredient (ingredientId) ON DELETE RESTRICT
 );
-
-CREATE TRIGGER card_checksum_insert
-BEFORE INSERT ON Member
-FOR EACH ROW
-WHEN NEW.cardNumber IS NOT NULL
-BEGIN
-    SELECT RAISE(ABORT, 'cardNumber must be exactly 10 digits')
-    WHERE LENGTH(NEW.cardNumber) != 10;
-
-    SELECT RAISE(ABORT, 'cardNumber must be comprised of only digits')
-    WHERE NEW.cardNumber NOT GLOB '*[^0-9]*';
-
-    SELECT RAISE(ABORT, 'cardNumber failed checksum validation')
-    WHERE (
-        10*CAST(substr(NEW.cardNumber,1,1) AS INTEGER) +
-         9*CAST(substr(NEW.cardNumber,2,1) AS INTEGER) +
-         8*CAST(substr(NEW.cardNumber,3,1) AS INTEGER) +
-         7*CAST(substr(NEW.cardNumber,4,1) AS INTEGER) +
-         6*CAST(substr(NEW.cardNumber,5,1) AS INTEGER) +
-         5*CAST(substr(NEW.cardNumber,6,1) AS INTEGER) +
-         4*CAST(substr(NEW.cardNumber,7,1) AS INTEGER) +
-         3*CAST(substr(NEW.cardNumber,8,1) AS INTEGER) +
-         2*CAST(substr(NEW.cardNumber,9,1) AS INTEGER) +
-         1*CAST(substr(NEW.cardNumber,10,1) AS INTEGER)
-    ) % 11 != 0;
-END;
-
-CREATE TRIGGER card_checksum_update
-BEFORE UPDATE ON Member
-FOR EACH ROW
-WHEN NEW.cardNumber IS NOT NULL
-BEGIN
-    SELECT RAISE(ABORT, 'cardNumber must be exactly 10 digits')
-    WHERE LENGTH(NEW.cardNumber) != 10;
-
-    SELECT RAISE(ABORT, 'cardNumber must be comprised of only digits')
-    WHERE NEW.cardNumber NOT GLOB '*[^0-9]*';
-
-    SELECT RAISE(ABORT, 'cardNumber failed checksum validation')
-    WHERE (
-        10*CAST(substr(NEW.cardNumber,1,1) AS INTEGER) +
-         9*CAST(substr(NEW.cardNumber,2,1) AS INTEGER) +
-         8*CAST(substr(NEW.cardNumber,3,1) AS INTEGER) +
-         7*CAST(substr(NEW.cardNumber,4,1) AS INTEGER) +
-         6*CAST(substr(NEW.cardNumber,5,1) AS INTEGER) +
-         5*CAST(substr(NEW.cardNumber,6,1) AS INTEGER) +
-         4*CAST(substr(NEW.cardNumber,7,1) AS INTEGER) +
-         3*CAST(substr(NEW.cardNumber,8,1) AS INTEGER) +
-         2*CAST(substr(NEW.cardNumber,9,1) AS INTEGER) +
-         1*CAST(substr(NEW.cardNumber,10,1) AS INTEGER)
-    ) % 11 != 0;
-END;
