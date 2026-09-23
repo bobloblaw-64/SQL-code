02b9287183ef5723038bd193d5ae8baebc9fb30d
bobloblaw-64 <fkthomson01@gmail.com>
2026-09-24 01:41:55 +0800

    Triggers Done

--- Project/createTriggers.sql
+++ Project/createTriggers.sql
@@ -1,9 +1,46 @@
 --By Finlay Thomson (23953297)
 
-CREATE TRIGGER price_calculator
+CREATE TRIGGER trga_price_calculator
 AFTER INSERT ON OrderItem
 FOR EACH ROW
 WHEN NEW.unitPrice IS NULL
 BEGIN
     UPDATE OrderItem
-    SET unitPrice = 
+    SET unitPrice = (
+        SELECT p.basePrice + s.sizeSurcharge
+        FROM Product p, SizeOption s
+        WHERE p.productId = NEW.productId
+        AND s.size = NEW.size
+    )
+    WHERE orderId = NEW.orderId 
+    AND lineNo = NEW.lineNo;
+END;
+
+CREATE TRIGGER trgb_free_drink
+AFTER INSERT ON OrderItem
+FOR EACH ROW
+WHEN NEW.lineTotal IS NULL
+BEGIN
+    UPDATE OrderItem
+    SET lineTotal = (
+        SELECT
+            CASE
+                WHEN (
+                    (
+                        SELECT SUM(quantity)
+                        FROM OrderItem
+                        JOIN SalesOrder USING (orderId)
+                        WHERE memberId = so.memberId
+                    ) % 10 < NEW.quantity
+                )
+                THEN (NEW.quantity - 1) * unitPrice
+                ELSE NEW.quantity * unitPrice
+            END
+        FROM OrderItem
+        JOIN SalesOrder so USING (orderId)
+        WHERE orderId = NEW.orderId
+        AND lineNo = NEW.lineNo
+    )
+    WHERE orderId = NEW.orderId
+    AND lineNo = NEW.lineNo;
+END;
