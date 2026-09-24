b914100166d6b847283fa43aa63b76a057d0aeb1
bobloblaw-64 <fkthomson01@gmail.com>
2026-09-24 13:51:50 +0800

    views done

--- Project/createViews.sql
+++ Project/createViews.sql
@@ -1,8 +1,29 @@
+--By Finlay Thomson (23953297)
 CREATE VIEW MonthlySales AS
 SELECT
-    AS orderMonth,
-    AS storeName,
-    AS productName,
-    AS totalQuantity,
-    AS totalRevenue
-FROM
+    SUBSTR(orderDate, 1, 7) AS orderMonth,
+    storeName,
+    productName,
+    SUM(quantity) AS totalQuantity,
+    SUM(lineTotal) AS totalRevenue
+
+FROM Store JOIN SalesOrder USING (storeId)
+JOIN OrderItem USING (orderId)
+JOIN Product USING (productId)
+
+WHERE lineTotal IS NOT NULL
+
+GROUP BY orderMonth, storeName, productName;
+
+CREATE VIEW MemberSummary AS
+SELECT
+    memberId,
+    memberName,
+    COUNT(DISTINCT orderId) AS totalOrders,
+    COALESCE(SUM(quantity), 0) AS totalDrinks,
+    COALESCE(SUM(lineTotal), 0) AS totalSpent
+
+FROM Member LEFT JOIN SalesOrder USING (memberId)
+LEFT JOIN OrderItem USING (orderId)
+
+GROUP BY memberId, memberName;
